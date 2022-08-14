75684ed47a23a4a980be978752ef1670f2e3f4cd
Llan Carlos Carlos <111029416+LlanGM09@users.noreply.github.com>
2022-08-14 21:37:18 +0000

    product new you added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {User} from './User.js'
-
+import {Product} from './Product.js'
 const LlanCarlos = new User('Llan Carlos', 'Gutierrez')
 console.log(LlanCarlos)
+console.log(new Product('laptop', 1500))
